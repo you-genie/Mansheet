fa6478b7b2a544fa4de9980d56f3338a1b48eca0
Genne Chung <krista2811@naver.com>
2019-05-23 19:35:31 +0900

    [#8] make color select component

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -61,30 +61,14 @@
               >
                 <template v-for="(event, i) in eventsMap[date]">
                   <!-- timed events -->
-                  <div
-                    v-if="event.start_time"
-                    :key="event.groupname"
-                    :style="{opacity: 0.5, top: timeToY(event.start_time) + 'px', height: minutesToPixels(event.duration) + 'px' }"
-                    class="my-event with-time"
-                    v-bind:class="[
-                      {'color1' : i%15 == 0},
-                      {'color2' : i%15 == 1},
-                      {'color3' : i%15 == 2},
-                      {'color4' : i%15 == 3},
-                      {'color5' : i%15 == 4},
-                      {'color6' : i%15 == 5},
-                      {'color7' : i%15 == 6}, 
-                      {'color8' : i%15 == 7},
-                      {'color9' : i%15 == 8},
-                      {'color10' : i%15 == 9},
-                      {'color11' : i%15 == 10},
-                      {'color12' : i%15 == 11},
-                      {'color13' : i%15 == 12},
-                      {'color14' : i%15 == 13},
-                      {'color15' : i%15 == 14}]"
-                    @click="open(event)"
-                    v-html="event.groupname"
-                  > </div>
+                  <color-event
+                    :i="i"
+                    :start_time="event.start_time"
+                    :groupname="event.groupname"
+                    :opacity="0.5"
+                    :timeToY="timeToY(event.start_time)"
+                    :minutesToPixels="minutesToPixels(event.duration)"
+                  />
                 </template>
               </template>
             </v-calendar>
@@ -95,103 +79,6 @@
     </div>
   </v-container>
 </template>
-<style lang="stylus" scoped>
-  .my-event {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    border-radius: 2px;
-    color: black;
-    font-size: 12px;
-    padding: 3px;
-    cursor: pointer;
-    margin-bottom: 1px;
-    left: 4px;
-    margin-right: 8px;
-    position: relative;
-
-    &.with-time {
-      position: absolute;
-      right: 4px;
-      margin-right: 0px;
-    }
-  }
-  .color1 {
-    background-color: #6b5b95;
-    border: 1px solid #6b5b95;
-    color: white;
-  }
-  .color2 {
-    background-color: #feb236;
-    border: 1px solid #feb236;
-    color: black;
-  }
-  .color3 {
-    background-color: #d64161;  
-    border: 1px solid #d64161;
-    color: black; 
-  }
-  .color4 {
-    background-color: #ff7b25;
-    border: 1px solid #ff7b25; 
-    color: black;      
-  }
-  .color5 {
-    background-color: #e3eaa7; 
-    border: 1px solid #e3eaa7;
-    color: black;      
-  }
-  .color6 {
-    background-color: #f7786b;
-    border: 1px solid #f7786b;
-    color: black;       
-  }
-  .color7 {
-    background-color: #b5e7a0;
-    border: 1px solid #b5e7a0;
-    color: black;      
-  }
-  .color8 {
-    background-color: #fefbd8;
-    border: 1px solid #fefbd8;
-    color: black;      
-  }
-  .color9 {
-    background-color: #ffef96;
-    border: 1px solid #ffef96;
-    color: black;       
-  }
-  .color10 {
-    background-color: #50394c;
-    border: 1px solid #50394c;
-    color: black;       
-  }
-  .color11 {
-    background-color: #4040a1;
-    border: 1px solid #4040a1;
-    color: black;       
-  }
-  .color12 {
-    background-color: #f4e1d2;
-    border: 1px solid #f4e1d2;
-    color: black;       
-  }
-  .color13 {
-    background-color: #ffcc5c;
-    border: 1px solid #ffcc5c;
-    color: black;       
-  }
-  .color14 {
-    background-color: #f18973;
-    border: 1px solid #f18973;
-    color: black;       
-  }
-  .color15 {
-    background-color: #ff6f69;
-    border: 1px solid #ff6f69;
-    color: black;       
-  }  
-</style>
 <script>
   import {
     mapState,
@@ -218,20 +105,15 @@
         return map
       }
     },
+    components: {
+      ColorEvent: () => import ('@/components/base/ColorEvent')
+    },
     mounted () {
       if (this.group == "all") {
         console.log("you need to implement this part")
       }
       this.$refs.calendar.scrollToTime('13:00'),
       this.fetchUserData
-    },
-    methods: {
-      open (event) {
-        alert(event.groupname)
-      },
-      getcolor (i) {
-        return this.color_preset[i]
-      }
     }
   }
 </script>
